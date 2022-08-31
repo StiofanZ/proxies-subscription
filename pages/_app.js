82e9aecb84d57058952af9df0994e499e752ec0b
Stiofan <damo120@vip.qq.com>
2022-08-31 21:49:49 +0800

    Thanks! This repo forked from https://github.com/qier222/proxy-provider-converter

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+﻿import "tailwindcss/tailwind.css";
+import "../styles/global.css";
+import splitbee from "@splitbee/web";
+
+function MyApp({Component, pageProps}) {
+    splitbee.init({
+        scriptUrl: "/bee.js",
+        apiUrl: "/_hive",
+    });
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
